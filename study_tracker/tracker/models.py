871a4901bfe91e6149c8363ac67ea178cf7fe03f
Bishwojit <forcoding55@gmail.com>
2026-04-26 12:17:42 +0600

    Creating StudySession Model

--- study_tracker/tracker/models.py
+++ study_tracker/tracker/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class StudySession(models.Model):
+    subject = models.CharField(max_length=100)
+    hours = models.FloatField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
